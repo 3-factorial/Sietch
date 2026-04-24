148156a4e7f56abe1520ba864bd9527e5d22da63
negativeboy666 <paco.puma@comunidad.unam.mx>
2026-04-24 13:34:43 -0600

    actu

--- sietch/microservice/models.py
+++ sietch/microservice/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Flux(models.Model):
+	date_time = models.DateTimeField("Date Time")
+	flux =  models.DecimalField(default = 0.0)
--- sietch/microservice/views.py
+++ sietch/microservice/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Flux
 # Create your views here.
 from django.http import HttpResponse
 
 def index(request):
+	now = datetime.now
+	flux = Flux(now, 1.0)
+	flux.save()
 	return HttpResponse("Este mi microservicio")
