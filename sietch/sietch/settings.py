07b51b595ef6e28712e09e0abc754b2334bb50f6
root <root@ip-172-31-18-17.ec2.internal>
2026-05-19 19:32:58 +0000

    sufriendo

--- sietch/sietch/settings.py
+++ sietch/sietch/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-c=s_brzxulo!&l=^=ggc4!oa6b+8$w2dec3vnmfkxm()cu0r5w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.220.113.170']
 
 
 # Application definition
@@ -72,10 +72,16 @@ WSGI_APPLICATION = "sietch.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME":  "testing2026",
+        'USER': 'mane',
+        'PASSWORD': '2401',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
